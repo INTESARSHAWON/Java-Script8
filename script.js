eecd79ce34d7283e5952de4a478a55e0956dd9ab
INTESARSHAWON <intesarshawon@gmail.com>
2024-04-10 13:20:07 +0600

    initial JS of DOM2

---
+++ script.js
@@ -0,0 +1,12 @@
+// JS theke Style a kivabe haat dea jay et niyei DOM2
+
+let div = document.querySelector("div");
+// console.log(div);
+
+div.style.backgroundColor= "white";
+div.style.backgroundColor= "purple";
+div.style.fontSize= "20px";
+div.innerText="Java Script";
+div.style.visibility= "hidden";
+
+// .style. evabe style a haat dea jay JS theke 
