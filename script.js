e917a6bc65f0727bd40ce2caf9b9ee61b091955e
INTESARSHAWON <intesarshawon@gmail.com>
2024-04-10 13:20:56 +0600

    append

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
-// JS theke Style a kivabe haat dea jay et niyei DOM2
+// // JS theke Style a kivabe haat dea jay et niyei DOM2
 
-let div = document.querySelector("div");
-// console.log(div);
+// let div = document.querySelector("div");
+// // console.log(div);
 
-div.style.backgroundColor= "white";
-div.style.backgroundColor= "purple";
-div.style.fontSize= "20px";
-div.innerText="Java Script";
-div.style.visibility= "hidden";
+// div.style.backgroundColor= "white";
+// div.style.backgroundColor= "purple";
+// div.style.fontSize= "20px";
+// div.innerText="Java Script";
+// div.style.visibility= "hidden";
 
-// .style. evabe style a haat dea jay JS theke 
+// // .style. evabe style a haat dea jay JS theke 
+
+
+
+// html, css a JS theke kivabe Access kora jay, Change kora jay eta dekhsi
+// Js teke kivabe add delete kora jay eta dekhbo ebar
+
+let newButton1= document.createElement("button");
+console.dir(newButton1);
+newButton1.innerText="I am append button";
+
+let div1 = document.querySelector("div");
+div1.append(newButton1); //node er pore jukto hoise
